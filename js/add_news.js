80356a1fa92d19d7e73131f750413ce9cf695478
Pavel Novitskiy <novipsh@outlook.com>
2018-01-15 11:43:11 +0200

    Lab_8 (adding responses/news with LS)

--- js/add_news.js
+++ js/add_news.js
@@ -1,76 +1,92 @@
 function isOnline() {
-  return window.navigator.onLine;
+    return window.navigator.onLine;
 }
 
-//Loading photo for future adding to news.html
-$(document).ready(function addImage(input, n) {
-  function readURL(input) {
-    if (input.files && input.files[0]) {
-      var reader = new FileReader();
+class News {
+    constructor(header, shortText, fullText, image) {
+        this.header = header;
+        this.shortText = shortText;
+        this.fullText = fullText;
+        this.image = image;
+    }
+}
 
-      reader.onload = function(e) {
-        $('#img').attr('src', e.target.result);
-      };
-      reader.readAsDataURL(input.files[0]);
+function getNews() {
+    var news = new Array;
+    var news_item = localStorage.getItem('news');
+    if (news_item !== null) {
+        news = JSON.parse(news_item);
     }
-  }
-  $("input[type=file]").change(function() {
-    readURL(this);
-  });
+    return news;
+}
 
-  //Check news fields info
-  $(document).on("click", "#send_news", function() {
+function addNews() {
+    var DEFAULT_PHOTO = "images/add_image.svg";
+    var imageForm = document.getElementById("userInputFile");
+    var newsHeader = document.getElementById("header");
+    var newsShortText = document.getElementById("shortText");
+    var newsText = document.getElementById("newsText");
+    var imagePreview = document.getElementById('user-image');
+    if (newsHeader.value == "") {
+        alert("Вкажіть заголовок статті");
+        return;
+    }
+    if (newsShortText.value == "") {
+        alert("Вкажіть короткий опис статті");
+        return;
+    }
+    if (newsText.value == "") {
+        alert("Вкажіть текст статті");
+        return;
+    }
+    if (imagePreview.src == DEFAULT_PHOTO) {
+        alert("Завантажте фото для статті");
+        return;
+    }
+    var news = new News(newsHeader.value, newsShortText.value, newsText.value, imagePreview.src);
+    if (isOnline()) {
+        //server stuff
+    }
+    addToStorage(news);
+    alert('Новина успішно опублікована! Оновіть сторінку з публікаціями');
+    newsHeader.value = "";
+    newsShortText.value = "";
+    newsText.value = "";
+    imagePreview.src = DEFAULT_PHOTO;
+    imageForm.value = "";
+}
 
-    var title = $("#title").val();
-    var description_short = $("#description_short").val();
-    var description_long = $("#description_long").val();
-    var error = false;
+function addToStorage(newsItem) {
+    var news = getNews();
+    news.push(newsItem);
+    localStorage.setItem('news', JSON.stringify(news));
+    return false;
+}
 
-    //Errors info
-    if (title.length > 100) {
-      document.getElementById("title").style.borderColor = "red";
-      $("#title").notify("Не більше 100 символів");
-      error = true;
-    }
-    if (title.length < 10) {
-      document.getElementById("title").style.borderColor = "red";
-      $("#title").notify("Не менше 10 символів");
-      error = true;
-    }
-    if (title.length == 0) {
-      document.getElementById("title").style.borderColor = "red";
-      $("#title").notify("Це поле має бути заповнене");
-      error = true;
-    }
-    if (description_short.length > 100) {
-      document.getElementById("description_short").style.borderColor = "red";
-      $("#description_short").notify("Не більше 100 символів");
-      error = true;
-    }
-    if (description_short.length == 0) {
-      document.getElementById("description_short").style.borderColor = "red";
-      $("#description_short").notify("Це поле має бути заповнене");
-      error = true;
-    }
-    if (description_long.length > 1000) {
-      document.getElementById("description_long").style.borderColor = "red";
-      $("#description_long").notify("Не більше 1000 символів");
-      error = true;
-    }
-    if (description_long.length == 0) {
-      document.getElementById("description_long").style.borderColor = "red";
-      $("#description_long").notify("Це поле має бути заповнене");
-      error = true;
-    }
-    if (error != true) {
-      document.getElementById("description_short").style.borderColor = "grey";
-      document.getElementById("title").style.borderColor = "grey";
-      alert("Новина успішно опублікована!");
-      $('#img').attr('src', "img/camera.png");
-      $("#description_short").val("");
-      $("#title").val("");
-    }
+function loadPreviewPhoto() {
+    var src = document.getElementById("userInputFile");
+    var target = document.getElementById("user-image");
+    var fr = new FileReader();
+    fr.readAsDataURL(src.files[0]);
+    fr.onload = function (e) {
+        target.src = this.result;
+    };
+}
 
+function createNews(news) {
+    var element = document.getElementById("newsRow");
+    element.innerHTML += '<div class="col-lg-4 card"> <center><img src = "' + news.image + '" alt = "News" width="300" height = "300"></center><center><h3>'
+        + news.header + '</h3></center><p>' + news.shortText + '</p></div>'
+}
 
-  });
-});
+function show() {
+    if (isOnline()) {
+        //server stuff
+    }
+    var news = getNews();
+    if ((typeof news !== 'undefined') && (news.length > 0)) {
+        for (var i = 0; i < news.length; i++) {
+            createNews(news[i]);
+        }
+    }
+}
--- js/add_response.js
+++ js/add_response.js
@@ -1,45 +1,87 @@
 function isOnline() {
   return window.navigator.onLine;
 }
-//Adding Response to response.html
+class Response {
+  constructor(name, response, date) {
+    this.name = name;
+    this.response = response;
+    this.date = date;
+  }
+}
+
+function addToStorage(response) {
+  var responses = getResponses();
+  responses.push(response);
+  localStorage.setItem('responses', JSON.stringify(responses));
+  //show();
+  return false;
+}
+
+function getResponses() {
+  var responses = new Array;
+  var response_item = localStorage.getItem('responses');
+  if (response_item !== null) {
+    responses = JSON.parse(response_item);
+  }
+  return responses;
+}
+
+function show() {
+  if (isOnline()) {
+    //server stuff
+  }
+  var responses = getResponses();
+  if ((typeof responses !== 'undefined') && (responses.length > 0)) {
+    for (var i = 0; i < responses.length; i++) {
+      createResponse(responses[i]);
+    }
+  }
+}
+
 function addResponse() {
   var responseText = document.getElementById("comment");
   var nameText = document.getElementById("name");
+  var date = new Date();
   if (nameText.value == "") {
-    document.getElementById("name").style.borderColor = "red";
-    $("#name").notify("Вкажіть ваше ім'я");
-    error = true;
+    alert("Вкажіть ваше ім'я");
     return;
   }
   if (responseText.value == "") {
-    document.getElementById("comment").style.borderColor = "red";
-    $("#comment").notify("Заповніть поле відгуку");
-    error = true;
+    alert("Порожній відгук!");
     return;
   }
-  //Get elements from file
+  var response = new Response(nameText.value, responseText.value, date);
+  if (isOnline()) {
+    //server stuff
+  }
+  addToStorage(response);
+  createResponse(response);
+  responseText.value = "";
+  nameText.value = "";
+}
+
+function createResponse(response) {
   var responseField = document.getElementById("newResponseField");
   var element = document.getElementById("responses");
-
-  //Initialize elements
+  var date = new Date(response.date);
+  var nameText = response.name;
+  var responseText = response.response;
   var responseRow = document.createElement("div");
-  responseRow.setAttribute("class", 'card');
+  responseRow.setAttribute("class", "row-lg");
   var responseCol = document.createElement("div");
+  responseCol.setAttribute("class", 'card');
   var responseHeader = document.createElement("p");
   var responseFill = document.createElement("p");
   var responseHeaderName = document.createElement("span");
   responseHeaderName.setAttribute("class", "h2 pull-left");
-  var responseHeaderDate = document.createElement("p");
   var responseHeaderDate = document.createElement("span");
   var responseHeaderDateItalic = document.createElement("i");
-  var date = new Date();
-  var dateString = date.getDate() + "." + (date.getMonth() + 1) + "." + (date.getFullYear()) +
+  var dateString = "";
+  dateString = date.getDate() + "." + (date.getMonth() + 1) + "." + (date.getFullYear()) +
     ", " + date.getHours() + ":" + date.getMinutes();
-
-  //Transfer data to addResponse()
   responseHeaderDateItalic.innerHTML = dateString;
-  responseHeaderName.innerHTML = nameText.value + " ";
-  responseFill.innerHTML = responseText.value;
+  responseHeaderName.innerHTML = nameText + " ";
+  responseFill.innerHTML = responseText;
   responseHeaderDate.appendChild(responseHeaderDateItalic);
   responseHeader.appendChild(responseHeaderName);
   responseHeader.appendChild(responseHeaderDate);
@@ -47,6 +89,6 @@ function addResponse() {
   responseCol.appendChild(responseFill);
   responseRow.appendChild(responseCol);
   element.insertBefore(responseRow, responseField);
-  responseText.value = "";
-  nameText.value = "";
+  element.insertBefore(document.createElement("hr"), responseField);
 }
+show();
